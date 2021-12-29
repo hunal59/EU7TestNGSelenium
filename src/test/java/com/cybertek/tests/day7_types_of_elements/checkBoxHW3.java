6e45b8a884f72dee39a8a72a5ee6c7b38d659cbf
hunal <hsynemr59@gmail.com>
2021-12-29 18:23:14 +0300

    day9 wait1

--- src/test/java/com/cybertek/tests/day7_types_of_elements/checkBoxHW3.java
+++ src/test/java/com/cybertek/tests/day7_types_of_elements/checkBoxHW3.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 
-public class checkBoxHW {
+public class checkBoxHW3 {
     public static void main(String[] args) throws InterruptedException {
 
         WebDriver driver = WebDriverFactory.getDriver("chrome");
@@ -25,7 +25,7 @@ public class checkBoxHW {
         WebElement checkall = driver.findElement(By.id("ctl00_MainContent_btnCheckAll"));
         checkall.click();
 
-        Thread.sleep(2000);
+        Thread.sleep(1000);
 
         WebElement one = driver.findElement(By.id("ctl00_MainContent_orderGrid_ctl02_OrderSelector"));
         WebElement two = driver.findElement(By.id("ctl00_MainContent_orderGrid_ctl03_OrderSelector"));
@@ -45,24 +45,60 @@ public class checkBoxHW {
         System.out.println("seven.isSelected() = " + seven.isSelected());
         System.out.println("eight.isSelected() = " + eight.isSelected());
 
-        Thread.sleep(2000);
+        Thread.sleep(1000);
 
         //4.
 
         WebElement uncheckbutton = driver.findElement(By.cssSelector("#ctl00_MainContent_btnUncheckAll"));
         uncheckbutton.click();
 
-        Thread.sleep(2000);
+        Thread.sleep(1000);
 
-        //Assert.assertFalse(one.isSelected(),"verify checkbox 1 is NOT selected");
+         one = driver.findElement(By.id("ctl00_MainContent_orderGrid_ctl02_OrderSelector"));
+         two = driver.findElement(By.id("ctl00_MainContent_orderGrid_ctl03_OrderSelector"));
+         three = driver.findElement(By.cssSelector("#ctl00_MainContent_orderGrid_ctl04_OrderSelector"));
+         four = driver.findElement(By.xpath("//input[@name='ctl00$MainContent$orderGrid$ctl05$OrderSelector']"));
+         five = driver.findElement(By.id("ctl00_MainContent_orderGrid_ctl06_OrderSelector"));
+         six = driver.findElement(By.id("ctl00_MainContent_orderGrid_ctl07_OrderSelector"));
+         seven = driver.findElement(By.id("ctl00_MainContent_orderGrid_ctl08_OrderSelector"));
+         eight = driver.findElement(By.id("ctl00_MainContent_orderGrid_ctl09_OrderSelector"));
+
+        Assert.assertFalse(one.isSelected());
+        Assert.assertFalse(two.isSelected());
+        Assert.assertFalse(three.isSelected());
+        Assert.assertFalse(four.isSelected());
+        Assert.assertFalse(five.isSelected());
+        Assert.assertFalse(six.isSelected());
+        Assert.assertFalse(seven.isSelected());
+        Assert.assertFalse(eight.isSelected());
+
+        Thread.sleep(1000);
 
 
         //5
-        three.click();
-        //WebElement deletebutton = driver.findElement(By.id("ctl00_MainContent_btnDelete"));
-        //deletebutton.click();
+
+        String expectedText = driver.findElement(By.xpath("//*[@id=\"ctl00_MainContent_orderGrid\"]/tbody/tr[2]/td[2]")).getText();
+
+        System.out.println("expectedText = " + expectedText);
+        Thread.sleep(1000);
+
+        one.click();
+
+        Thread.sleep(1000);
+
+        WebElement deletebutton = driver.findElement(By.id("ctl00_MainContent_btnDelete"));
+        deletebutton.click();
+
+        Thread.sleep(1000);
 
 
+        WebElement newButton1= driver.findElement(By.xpath("//*[@id=\"ctl00_MainContent_orderGrid\"]/tbody/tr[2]/td[2]"));//6 to verify
+        String actualText= newButton1.getText(); //6 to verify
+        System.out.println("actualText = " + actualText);
+
+        Assert.assertFalse(actualText.equals(expectedText)); //6
+
+        driver.quit();
 
 
     }
