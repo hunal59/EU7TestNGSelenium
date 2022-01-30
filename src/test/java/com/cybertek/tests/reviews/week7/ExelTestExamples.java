06aa7c0e529132b18daad8268af76538b939b578
hunal <hsynemr59@gmail.com>
2022-01-30 22:34:30 +0300

    week7

--- src/test/java/com/cybertek/tests/reviews/week7/ExelTestExamples.java
+++ src/test/java/com/cybertek/tests/reviews/week7/ExelTestExamples.java
@@ -1,8 +1,16 @@
 package com.cybertek.tests.reviews.week7;
 
 import com.cybertek.utilities.ExcelUtil;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.testng.annotations.Test;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -12,7 +20,7 @@ public class ExelTestExamples {
 
     List<Map<String,String>> users = new ArrayList<>();
     @Test
-    public void createExcelFile(){
+    public void createExcelFile() throws IOException {
         ExcelUtil readFile = new ExcelUtil("src/test/resources/Vytracktestdata.xlsx","QA3-short");
 
         int rowCount = readFile.rowCount(); // This one gives how many rows I have
@@ -32,6 +40,37 @@ public class ExelTestExamples {
         System.out.println("users = " + users);
 
         System.out.println("users.get(5).get(\"password\") = " + users.get(5).get("firstname")); // points to line 7, gives the 6th person
+
+        // Creating and writing to an excel file
+        Workbook workbook = new XSSFWorkbook();
+
+        Sheet sheet = workbook.createSheet("MyUsers");
+
+        Row headerRow = sheet.createRow(0);
+        for (int i = 0; i < readFile.columnCount() ; i++) {
+            Cell cell = headerRow.createCell(i);                   // create a cell
+            cell.setCellValue(readFile.getColumnsNames().get(i));  // put data in the cell
+        }
+
+        for (int i = 1; i < readFile.rowCount()-1; i++) {
+            Row eachRow = sheet.createRow(i);
+            eachRow.createCell(0).setCellValue(users.get(i).get("username"));
+            eachRow.createCell(1).setCellValue(users.get(i).get("password"));
+            eachRow.createCell(2).setCellValue(users.get(i).get("firstname"));
+            eachRow.createCell(3).setCellValue(users.get(i).get("lastname"));
+        }
+
+        FileOutputStream fileOutputStream = new FileOutputStream("src/test/resources/MyUsers.xlsx");
+
+        workbook.write(fileOutputStream);
+        fileOutputStream.close();
+        workbook.close();
+        /*
+        3 more weeks for UI Automation
+        2 weeks for DataBase (easiest topic)
+        3 weeks for API (Mobile Testing as optional videos)
+         */
+
     }
 
 }
